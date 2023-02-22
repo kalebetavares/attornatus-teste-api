4078d845c64276d079b6996e0547ec6b473e7cda
Kalebe Tavares <kalebe.tavares@gmail.com>
2023-02-21 22:46:00 -0300

    Atualização das configurações do Swagger

--- src/main/java/attornatus/teste/api/config/SwaggerConfig.java
+++ src/main/java/attornatus/teste/api/config/SwaggerConfig.java
@@ -1,6 +1,14 @@
 package attornatus.teste.api.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
@@ -9,7 +17,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.demo"))
+                .apis(RequestHandlerSelectors.basePackage("attornatus.teste.api.controller"))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo());
@@ -17,8 +25,8 @@ public class SwaggerConfig {
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("My API")
-                .description("Description of my API")
+                .title("API REST teste técnico")
+                .description("API desenvolvida para teste técnico para avaliar os conhecimentos em Java/Spring")
                 .version("1.0")
                 .build();
     }
